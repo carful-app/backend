fa0d7d30cbbab05147f4009cb632ea01404f44a2
Mihail Georgiev <mmisho0501@gmail.com>
2023-03-26 20:01:08 +0300

    Add auth validators

---
+++ app/GraphQL/Validators/RegisterInputValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\GraphQL\Validators;
+
+use Nuwave\Lighthouse\Validation\Validator;
+
+final class RegisterInputValidator extends Validator
+{
+    /**
+     * Return the validation rules.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+
+    /**
+     * Return the validation attributes.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+        ];
+    }
+}
